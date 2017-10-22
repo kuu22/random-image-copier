0361891a94ecc66f4f401721d0c1ff4c7caeb655
kuu22 <pabloalonso998@gmail.com>
2017-10-22 21:58:04 +0200

    Compare only changed pixels

--- ImageCopier/src/es/deusto/prog3/extra/Main.java
+++ ImageCopier/src/es/deusto/prog3/extra/Main.java
@@ -22,7 +22,6 @@ import es.deusto.prog3.extra.utils.Utils;
  * - Compare both images to the source image. 
  * - If it's closer to the source image than image2, copy image1 to image2; if not, copy image2 to image1 and repeat the process a number of iterations.
  * 
- * Note: performance is very slow, only use with smaller images
  * @author Pablo Alonso
  *
  */
@@ -56,37 +55,43 @@ public class Main {
 		
 		JFrame frame = new JFrame();
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setSize(source.getWidth(), source.getHeight());
+		frame.setSize(source.getWidth(), source.getHeight() + 10);
 		Canvas canvas = new Canvas();
 		canvas.setSize(source.getWidth(), source.getHeight());
 		frame.getContentPane().add(canvas);
 		frame.setVisible(true);
 		new Thread() {
 			public void run() {
-				for (int i = 0; i < iterations; i++) {
+				for (int i = 1; i < iterations; i++) {
+					
 					Graphics g1 = image1.getGraphics();
 					g1.setColor(new Color(colors.get(Utils.randomInt(colors.size() - 1))));
-					g1.fillOval(Utils.randomInt(image1.getWidth()), Utils.randomInt(image1.getHeight()), circleRadius, circleRadius);
+					int x = Utils.randomInt(image1.getWidth());
+					int y = Utils.randomInt(image1.getHeight());
+					g1.fillOval(x, y, circleRadius, circleRadius);
 					g1.dispose();
-					double diffToSource1 = getDiffPercent(image1, source);
-					double diffToSource2 = getDiffPercent(image2, source);
+					
+					long diffToSource1 = getDiffPercent(image1, source, x, y);
+					long diffToSource2 = getDiffPercent(image2, source, x, y);
 					
 					if(diffToSource1 < diffToSource2) {
-						image2 = new BufferedImage(image1.getWidth(), image1.getHeight(), image1.getType());
 						Graphics g = image2.getGraphics();
 						g.drawImage(image1, 0, 0, null);
 						g.dispose();
 					} else {
-						image1 = new BufferedImage(image2.getWidth(), image2.getHeight(), image2.getType());
 						Graphics g = image1.getGraphics();
 						g.drawImage(image2, 0, 0, null);
 						g.dispose();
 					}
-					if(i % 1000 == 0) {
+					
+					if(i % 100 == 0) {
 						Graphics canvasG = canvas.getGraphics();
 						canvasG.drawImage(image1, 0, 0, null);
 						canvasG.dispose();
-						System.out.println(i);
+						if(i % (iterations/circleRadius) == 0) {
+							System.out.println(i);
+							circleRadius--;
+						}
 					}
 					
 				}
@@ -96,20 +101,22 @@ public class Main {
 		
 	}
 	
-	public static double getDiffPercent(BufferedImage img1, BufferedImage img2) {
-		// TODO needs optimization
-		int height = img1.getHeight();
-		int width = img1.getWidth();
+	public static long getDiffPercent(BufferedImage img1, BufferedImage img2, int x, int y) {
 		long diff = 0;
-		for (int i = 0; i < width; i++) {
-			for (int j = 0; j < height; j++) {
-				diff += pixelDiff(img1.getRGB(i, j), img2.getRGB(i, j));
+		for (int i = x; i < x + circleRadius; i++) {
+			if(i == img1.getWidth()) {
+				break;
+			}
+			for(int j = y; j < y + circleRadius; j++) {
+				if(j == img1.getHeight()) {
+					break;
+				}
+				diff+= pixelDiff(img1.getRGB(i, j), img2.getRGB(i, j));
+				
 			}
 		}
 		
-		long maxDiff = 3L * 255 * width * height;
-		
-		return 100.0 * diff/maxDiff;
+		return diff;
 	}
 	
 	public static int pixelDiff(int a, int b) {
